e626a4279993e62861ac7c21860779f5bed193bc
lordprasun <lordprasun@gmail.com>
2018-09-14 17:48:30 +0530

    Create Account and Get Account Details Working

--- CreateAccount.py
+++ CreateAccount.py
@@ -1,9 +1,31 @@
+import pymysql
 def createAccount(AccountDetails):
-    count = 1
-    FirstName = AccountDetails["FirstName"]
-    LastName = AccountDetails["LastName"]
-    Name = (FirstName + LastName).upper()
-    DateOfBirth = AccountDetails["DateOfBirth"]
-    Address = AccountDetails["Address"]
-    AccountNo = Name[:4]+ DateOfBirth + ("{:05d}".format(count))
+    try:
+        count = 1
+        FirstName = AccountDetails["FirstName"]
+        LastName = AccountDetails["LastName"]
+        Name = (FirstName + LastName).upper()
+        DateOfBirth = AccountDetails["DateOfBirth"]
+        Address = AccountDetails["Address"]
+        Email = AccountDetails["Email"]
+        ContactNo= AccountDetails["ContactNo"]
+        AccountNo = Name[:4]+ DateOfBirth + ("{:05d}".format(count))
+        connection =pymysql.connect('accountmngsystem.mysql.pythonanywhere-services.com','accountmngsystem','Postgres','accountmngsystem$gbs')
+        cursor=connection.cursor()
+        cursor.execute("Insert into account (AccountNo,FirstName,LastName,DOB,Address,Email,PhoneNo) values (%s,%s,%s,%s,%s,%s,%s)",(AccountNo,FirstName,LastName,DateOfBirth,Address,Email,ContactNo))
+        connection.commit()
+        connection.close()
+    except Exception as e:
+        AccountNo = "Error Creating Account ", e
     return AccountNo
+
+def getAccount(AccountNo):
+    try:
+        connection =pymysql.connect('accountmngsystem.mysql.pythonanywhere-services.com','accountmngsystem','Postgres','accountmngsystem$gbs')
+        cursor=connection.cursor()
+        cursor.execute("Select * from account where AccountNo='{}'".format(AccountNo))
+        data=cursor.fetchall()[0]
+    except Exception as e:
+        data = "Error Fetching Account Details",e
+    return data
+
--- LaunchApp.py
+++ LaunchApp.py
@@ -14,13 +14,28 @@ def createaccount():
         LastName = request.form['lastname']
         DateOfBirth = datetime.datetime.strptime(request.form['dob'], "%Y-%m-%d")
         DateOfBirth = DateOfBirth.strftime("%j%y")
-        Address = request.form['lastname']
+        Address = request.form['address']
+        Email = request.form['email']
+        ContactNo = request.form['contactno']
         AccountDetails = {"FirstName":FirstName,"LastName":LastName,
-                          "DateOfBirth":DateOfBirth,"Address":Address}
+                          "DateOfBirth":DateOfBirth,"Address":Address,
+                          "Email":Email,"ContactNo":ContactNo}
         AccountNumber = CreateAccount.createAccount(AccountDetails)
         return AccountNumber
+
     else:
-        render_template('registration.html')
+        return render_template('registration.html')
+
+@app.route('/getaccount/<AccountNumber>',methods =['POST','GET'])
+def getaccount(AccountNumber):
+    AccData = CreateAccount.getAccount(AccountNumber)
+    AccountDetails = '<h1> Account Details </h1><br>'
+    AccountDetails += '<b>Name:</b><p>{}{}<p><br>'.format(AccData[1],AccData[2])
+    AccountDetails += '<b>Account Number:</b><p>{}<p><br>'.format(AccData[0])
+    AccountDetails += '<b>Address:</b><p>{}<p><br>'.format(AccData[4])
+    AccountDetails += '<b>Email:</b><p>{}<p><br>'.format(AccData[5])
+    AccountDetails += '<b>Phone:</b><p>{}<p><br>'.format(AccData[6])
+    return AccountDetails
 @app.route('/hello/<name>')
 def hello_name(name):
    return 'Hello %s!' % name
